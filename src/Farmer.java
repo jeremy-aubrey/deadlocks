9ab7dd2babeebfa7f6b428683bfc007abaa4dc06
jeremy aubrey <jeremyaubrey@gmail.com>
2022-04-14 13:54:03 -0500

    farmers cross road synchronously

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,4 +1,48 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Farmer {
+public class Farmer implements Runnable {
+
+	private static int farmerCount;
+	private static Lock road = new ReentrantLock();
+	private int id;
+	private String direction;
+	
+	public Farmer(String direction) {
+		id = ++farmerCount;
+		this.direction = direction;
+	}
+	
+	private void declareEntry() {
+		System.out.println(direction+" Farmer "+id+" is in the tunnel.");
+	}
+	
+	private void declareExit() {
+		System.out.println(direction+" Farmer "+id+" is exiting the tunnel.");
+	}
+	
+	
+	private void crossRoad() {
+		
+		System.out.println(direction+" Farmer "+id+" wants to enter the tunnel.");
+		while(!road.tryLock());
+		declareEntry();
+		try {
+			Thread.sleep((int)(5000 * Math.random()) + 1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			declareExit();
+			road.unlock();
+		}
+	}
+	
+	@Override
+	public void run() {
+		
+		crossRoad();
+
+	}
 
 }
--- src/FarmerTest.java
+++ src/FarmerTest.java
@@ -1,13 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FarmerTest {
 
+	private static final int FARMER_COUNT = 50;
+	
 	public static void main(String[] args) {
 		
 		FarmerTest obj = new FarmerTest();
 		obj.developerInfo();
 		
+		List<Farmer> farmers = new ArrayList<>();
+		int coreCount = Runtime.getRuntime().availableProcessors();
+		ExecutorService pool = Executors.newFixedThreadPool(coreCount);
 		
+		String direction = "";
 		
+		// add runnable farmers to list
+		for(int i = 0; i < FARMER_COUNT; i++) {
+			if(i % 2 == 0) {
+				direction = "NorthBound";
+			} else {
+				direction = "SouthBound";
+			}
+			farmers.add(new Farmer(direction));
+		}
+		
+		// submit farmers to thread pool
+		for(int i = 0; i < farmers.size(); i++) {
+			pool.submit(farmers.get(i));
+			try {
+				Thread.sleep((int)(1000 * Math.random()) + 1); //simulate offset arrival
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		// shutdown
+		pool.shutdown();
+		try {
+			pool.awaitTermination(1, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
     //***************************************************************
